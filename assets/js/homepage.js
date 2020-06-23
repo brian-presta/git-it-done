9d4d4808c377c822d31e62813fb852692c82ac54
brian presta <brian.presta@gmail.comgit commit -m first>
2020-06-22 17:17:37 -0700

    repo data now displays on page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,49 @@
+var userForm = document.querySelector("#user-form")
+var nameInput = document.querySelector("#username")
+var repoContainer = document.querySelector('#repos-container')
+var repoSearchTerm = document.querySelector("#repo-search-term")
 var getUserRepos = function(user) {
     var apiUrl = `https://api.github.com/users/${user}/repos`
     fetch(apiUrl).then(function(response){
-        response.json().then(function(data){
-            console.log(data)
-        })
+        if (response.ok) {
+            response.json().then(function(data){
+            displayRepos(data,user)
+        })}
+        else {
+            repoContainer.textContent = 'User not found.'
+        }
+    })
+    .catch(function(error) {
+        alert("Unable to connect to GitHub")
     })
 }
-var userForm = document.querySelector("#user-form")
-var nameInput = document.querySelector("#username")
+var displayRepos = function(repos,searchTerm) {
+    if (repos.lenght < 1) {
+        repoContainer.textContent = 'No repositories found.'
+        return
+    }
+    repoContainer.textContent = ''
+    repoSearchTerm.textContent = searchTerm
+    repos.forEach( x => {
+        var repoName = x.owner.login + '/' + x.name
+        var repo = document.createElement('div')
+        repo.classList = 'list-item flex-row justify-space-between align-center'
+        var title = document.createElement('span')
+        title.textContent = repoName
+        repo.appendChild(title)
+        var status = document.createElement('span')
+        status.classList = 'flex-row align-center'
+        if (x.open_issues_count > 0){
+            status.innerHTML = 
+            "<i class='fas fa-times status-icon icon-danger'></i>" + x.open_issues_count + " issue(s)"
+        }
+        else {
+            status.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+        repo.appendChild(status)
+        repoContainer.appendChild(repo)
+    });
+}
 var formSubmitHandler = function(event) {
     event.preventDefault()
     var username = nameInput.value.trim()
